0d364ccd672930484274ceb0952fa704d242e9cb
FilipeGuto <filipeguto95@gmail.com>
2022-07-22 23:24:10 -0300

    FEAT: Deploy Heroku

--- Challenge/Api/server.js
+++ Challenge/Api/server.js
@@ -1,5 +1,9 @@
+const http = require('http');
 const app = require('./app');
 
-const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Connecting to ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Connecting to ${PORT}`));
